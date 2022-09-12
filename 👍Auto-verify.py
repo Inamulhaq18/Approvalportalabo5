ad7003ab75aecf1ab11c6fb6b09ee987ad4ad5b5
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-09-12 13:04:28 +0300

    Update 👍Auto-verify.py

--- 👍Auto-verify.py
+++ 👍Auto-verify.py
@@ -16,7 +16,7 @@ def skip():
         curr.execute(sql_select_queryskip, (status,str(product_id),))
         conn.commit()
 #initialize the database connection
-if st.button("Skip if you see error"):
+if st.button("Skip if you see an error"):
     skip()
 conn=psycopg2.connect("postgresql://hkmuctkbhmlhsr:59563300aab6c650f8bbc9cc4153df6a42054b71e9be00dda420f40bbbf791b2@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dd8a5bspvhrk8c") 
 curr=conn.cursor()
