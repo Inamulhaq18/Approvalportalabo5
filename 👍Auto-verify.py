4fe939f32262a8927186406b5c0fcfe21c6424b5
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-09-18 11:11:51 +0300

    Update 👍Auto-verify.py

--- 👍Auto-verify.py
+++ 👍Auto-verify.py
@@ -173,7 +173,14 @@ with st.expander('rotate images'):
        rotatethese = st.multiselect('Select images that are to be rotated',lst)
        direction = st.selectbox("Select the direction to rotate",["Left", "Right"])
        if st.button("Process"):
-        st.write("HEllo")
+         for item in lst:
+             if "R" in str(item):
+                item=item.replace("R","")
+                item=int(item)
+                Product_image_P_url=Product_image_P_url.split(",")
+                image_to_process=Product_image_P_url[item]
+                st.image("yoyo",image_to_process)
+                
 
 if varient!= None:
     with st.expander("Varient", expanded=True):
