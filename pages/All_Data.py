f3c5045822e5abfcaf3477889d21c4ef6fe3703a
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-09-18 13:42:10 +0300

    Update All_Data.py

--- pages/All_Data.py
+++ pages/All_Data.py
@@ -1,6 +1,7 @@
 import psycopg2
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid
 
 conn=psycopg2.connect("postgresql://hkmuctkbhmlhsr:59563300aab6c650f8bbc9cc4153df6a42054b71e9be00dda420f40bbbf791b2@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/dd8a5bspvhrk8c") 
 curr=conn.cursor()
@@ -10,4 +11,4 @@ curr=conn.cursor()
 sql = "SELECT * FROM master_product_table"
 dat = pd.read_sql_query(sql,conn)
 
-st.dataframe(dat)
+AgGrid(dat)
