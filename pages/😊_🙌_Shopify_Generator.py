db683012de6a976782bf017c26e0cc10df7b6554
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-09-25 17:11:00 +0300

    Update 😊_🙌_Shopify_Generator.py

--- pages/😊_🙌_Shopify_Generator.py
+++ pages/😊_🙌_Shopify_Generator.py
@@ -45,6 +45,9 @@ pfa=pfa.dropna(subset=['variety'])
 def getrowlen(row):   
    if "data" in row['variety']:
       rowlen=len(row['variety']['data'])
+   if "imgsource" in row ['variety']:
+      imgsourcelen=len(row['variety']['imgsource'])
+      rowlen=max(rowlen,imgsourcelen)
    else:
       rowlen=1
 
